e8f1bcedb7ed48f5aba3f7729313b184d72a1a2a
mosesmgk <mlmamoses2@gmail.com>
2021-06-16 18:06:30 -0700

    Res Changes

--- cst438Car-G15/src/main/java/cst438/controllers/CarController.java
+++ cst438Car-G15/src/main/java/cst438/controllers/CarController.java
@@ -25,6 +25,9 @@ public class CarController {
 	@Autowired
 	CarService carService;
 	
+	
+	
+	//List of all cars in DB
 	@GetMapping("/allCars")
 	public String getAllCars(Model model) {
 		List<Car> cars_list = CarRepository.findByPrice();
@@ -33,7 +36,7 @@ public class CarController {
 		return "cars_list";
 		
 	}
-	
+	//Landing Page
 	@GetMapping("/")
 	public String getIndex(Model model) {
 		Reservation reservation = new Reservation();
@@ -46,13 +49,21 @@ public class CarController {
 	//Page to filter cars by City wanted by customer
     @PostMapping("/select_reservationByCity")
     public String getCarByCity(@RequestParam("city") String city, Model model ) throws ParseException {
-    	System.out.println("Before: ");
-        List<Car> cars = carService.getCarByCity(city);
-        model.addAttribute("car", cars);
-        System.out.println("Cars info: ");
-        return "reservation_page";
+        List<Car> cars = CarRepository.findByCity(city);
+        model.addAttribute("Car", cars);
+        System.out.append(city);
+        return "reservations_page";
     }
-      
-	
+      //try using findbyid from the carrepo page .java 
+	@PostMapping("/select/reservation_page")
+	public String getCarById(@RequestParam("id") int id, Model model) {
+		 List<Car> test= CarRepository.findById(id);
+		Car cars = new Car();
+		//Car cars = CarRepository.findCarById(id);
+		model.addAttribute("Car", test);
+		model.addAttribute("car", cars);		
+			return "reservations_save";
+	}
 
+	//@PostMapping("/select/confirmation")
 }
